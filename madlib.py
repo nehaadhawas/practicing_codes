11acdb2b9c94716b8678977efe3cea651c98af07
Nehaa <157296420+nehaadhawas@users.noreply.github.com>
2024-10-14 23:27:16 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+adj = input("adj :")
+verb1 = input("verb:")
+verb2 = input("verb:")
+verb3 = input("verb:")
+
+madlib = f"""Heyy, I am so {adj},and I was waiting for you like I
+have decided so many {verb1} so be ready and watch {verb2}
+.btw I was worried about yesterday and {verb3},thanks."""
+
+print(madlib)
